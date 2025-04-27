c86fcfa422b12d2f26140e3037e629da608d6063
Yulia <yulia@example.com>
2025-04-27 16:59:44 +0600

    Обновил код: исправил вебхук и скрипты-8

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ app.post('/api/booking', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
